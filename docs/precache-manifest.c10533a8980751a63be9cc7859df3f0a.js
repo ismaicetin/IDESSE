da97e1db3ee5fbadd3ecc990e61d51082ab32772
ismail <ismaicetin@hotmail.com>
2020-02-27 22:48:46 +0300

    guncelleme

---
+++ docs/precache-manifest.c10533a8980751a63be9cc7859df3f0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "743fe65df4ea0932b6d1672903986569",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b1618a0856f47ee993c",
+    "url": "/static/css/main.7c0ab568.chunk.css"
+  },
+  {
+    "revision": "3fa1f7dcb33e400f60c3",
+    "url": "/static/js/2.0061256c.chunk.js"
+  },
+  {
+    "revision": "971cdbd360bcec66e32b11183bde9fbf",
+    "url": "/static/js/2.0061256c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b1618a0856f47ee993c",
+    "url": "/static/js/main.e32a5aa2.chunk.js"
+  },
+  {
+    "revision": "28388b63ac23ee6f38c7",
+    "url": "/static/js/runtime-main.c9f319e9.js"
+  }
+]);
