f5e48158a99596f10efb361c08df381a4ee92e9a
ismail <ismaicetin@hotmail.com>
2020-02-27 23:06:41 +0300

    Updates

---
+++ precache-manifest.71dfc8aa938a15a9ad4c21c53acbbafb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5564207bcb9ab6d5df4b0b4044b7129",
+    "url": "/IDESSE/index.html"
+  },
+  {
+    "revision": "9b1618a0856f47ee993c",
+    "url": "/IDESSE/static/css/main.7c0ab568.chunk.css"
+  },
+  {
+    "revision": "3fa1f7dcb33e400f60c3",
+    "url": "/IDESSE/static/js/2.0061256c.chunk.js"
+  },
+  {
+    "revision": "971cdbd360bcec66e32b11183bde9fbf",
+    "url": "/IDESSE/static/js/2.0061256c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b1618a0856f47ee993c",
+    "url": "/IDESSE/static/js/main.e32a5aa2.chunk.js"
+  },
+  {
+    "revision": "a87c7d5156ec6dc3adcd",
+    "url": "/IDESSE/static/js/runtime-main.0e443ff1.js"
+  }
+]);
